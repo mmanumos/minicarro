cbf9ac89d566218b7a7eb099d63bc66dc0ffdb10
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-08-16 00:07:54 -0500

    [Feature] Comments to code

--- products/views/login.py
+++ products/views/login.py
@@ -4,6 +4,29 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import logout as do_logout, login as do_login
 
 
+def login(request):
+    """ login validation for the user """
+    # Validation if user is authenticated
+    if request.user.is_authenticated:
+        return redirect('/')
+    # Setting a form
+    form = AuthenticationForm()
+    # Validation data for the user
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            # Getting the user
+            user = authenticate(username=username, password=password)
+            # if user exists
+            if user is not None:
+                do_login(request, user)
+                return redirect('/')
+    # render for the login form
+    return render(request, 'login.html', {'form': form})
+
+
 def register(request):
     """ View to register a new user """
     # Validation if user is authenticated
@@ -24,29 +47,6 @@ def register(request):
                 return redirect('/home')
     # render form to create user
     return render(request, 'register.html', {'form': form})
-
-
-def login(request):
-    """ login validation for the user """
-    # Validation if user is authenticated
-    if request.user.is_authenticated:
-        return redirect('/')
-     # Setting a form
-    form = AuthenticationForm()
-    # Validation data for the user
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            # Getting the user
-            user = authenticate(username=username, password=password)
-            # if user exists
-            if user is not None:
-                do_login(request, user)
-                return redirect('/')
-    # render for the login form
-    return render(request, 'login.html', {'form': form})
 
 
 def logout(request):
